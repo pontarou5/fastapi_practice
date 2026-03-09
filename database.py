f3397ba80c7d6d4b591883cbcc5ebf6ee10660c9
pontarou5 <miyu382155@gmail.com>
2026-03-10 00:31:28 +0900

    Initial version: FastAPI user API with SQLite

---
+++ database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+DB_NAME ="users.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            name TEXT,
+            followers INTEGER
+            )
+            """)
+    conn.commit()
+    conn.close()
+    
---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+import requests
+import sqlite3
+from database import init_db
+
+app = FastAPI()
+
+init_db()
+
+DB_NAME = "users.db"
+
+@app.get("/")
+def root():
+    return {"message": "API running"}
+
+@app.get("/fetch_user")
+def fetch_user():
+    #GitHub API
+    url = "https://api.github.com/users/octocat"
+    res = requests.get(url)
+
+    data = res.json()
+
+    name = data["login"]
+    followers = data["followers"]
+
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO users VALUES (?, ?)",
+        (name, followers)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return {
+        "name": name,
+        "followers": followers
+    }
+
+@app.get("/users")
+def get_users():
+
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users")
+    
+    rows = cur.fetchall()
+
+    conn.close()
+
+    return {"users": rows}
